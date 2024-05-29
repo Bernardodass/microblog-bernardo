c4e332e148779c256f2b96260fb38385d08164d8
Bernardodass <bernardo113322@gmail.com>
2024-05-29 16:16:03 -0300

    ...

---
+++ inc/conecta.php
@@ -0,0 +1,25 @@
+<?php // Script de conexão com o servidor de banco de dados. Necessário para que o Microblog possa usar o MySQL
+
+// parametros para conexão
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "microblog_bernardo";
+
+// função para conexão com o servidor de banco de dados
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+// definindo o charset da conexão 
+mysqli_set_charset($conexao, "utf8");
+
+
+// fazendo o teste de conexão
+if(!$conexao){
+    // deu problema? "mate/pare" a aplicação!
+    die("Ferrou:" .mysqli_connect_error());
+} else{
+    echo "Beleza, conectando...";
+}
+
+
+?>

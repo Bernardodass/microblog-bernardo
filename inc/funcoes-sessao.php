5f5f4d442b6930707bfdca82b15d714e65bf6995
Bernardodass <bernardo113322@gmail.com>
2024-06-04 17:22:58 -0300

    Create funcoes-sessao.php

---
+++ inc/funcoes-sessao.php
@@ -0,0 +1,25 @@
+<?php
+/* Sessões no PHP
+Funcionalidade usado para o controle de acesso à determinadas páginas e/ou recursos de site.
+
+Exemplos: área administrativa, painel de controle, área de cliente/aluno etc.
+
+Nestas áreas o acesso só é possivel mediante alguma forma de autenticação. Exemplos: login/senha, digital, facial, etc. */
+
+// verificando se não há/existe uma sessão em funcionamento 
+if( !isset($_SESSION) ){
+    // se não tem, inicie uma sessão
+    session_start();
+}     
+
+function verificaAcesso(){
+    /* Se NÃO EXISTIR uma variável de sessão chamada "id" (baseada nos ids usuários do banco), então significa que o usuário NÃO ESTÁ LOGADO. */
+    if(!isset($_SESSION['id'])){
+        // portanto, destruimos a sessão
+        session_destroy();
+        // fazemos o usuário voltar para a página login
+        header("location:.../login.php");
+        // paramos qualquer outra execução/processamento
+        exit; // ou die()
+    }
+}

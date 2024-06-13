8e7e195ba6e61bea618cdcdf33c49461505e7b67
Bernardodass <bernardo113322@gmail.com>
2024-06-13 13:51:43 -0300

    Update funcoes-noticias.php

--- inc/funcoes-noticias.php
+++ inc/funcoes-noticias.php
@@ -137,7 +137,11 @@ function lerNoticiaCompleta($conexao, $id)
 }
 
 // resultados.php
-function busca($conexao)
+function busca($conexao, $termoDigitado)
 {
-
+    // %% (operador coringa) a palavra pesquisada pode estar em qualquer lugar e o LIKE em vez do = para entender essa possibilidade
+ 
+    $sql = "SELECT id, data, titulo, resumo FROM noticias WHERE titulo LIKE '%$termoDigitado%' OR resumo LIKE '%$termoDigitado%' OR texto LIKE '%$termoDigitado%' ORDER BY DATA DESC";
+    $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+    return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 }
